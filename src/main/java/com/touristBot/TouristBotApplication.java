4df90cb60f93a82c5120759e5322b1cf79389f8f
den7by@mail.ru <den7by@mail.ru>
2021-03-17 14:19:12 +0300

    create project

---
+++ src/main/java/com/touristBot/TouristBotApplication.java
@@ -0,0 +1,13 @@
+package com.touristBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristBotApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/databaseScripts/create_city_book.sql
@@ -0,0 +1,5 @@
+CREATE TABLE city_book(
+	city_book_id serial PRIMARY KEY,
+	city VARCHAR(50) NOT NULL,
+	description VARCHAR(200)
+)
