481a1f81a6d886408d1292511dee987646dde7ef
den7by@mail.ru <den7by@mail.ru>
2021-03-17 17:20:55 +0300

    create demo bot

--- src/main/java/com/touristBot/TouristBotApplication.java
+++ src/main/java/com/touristBot/TouristBotApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TouristBotApplication {
 
 	public static void main(String[] args) {
+		ApiContextInitializer.init();
 		SpringApplication.run(TouristBotApplication.class, args);
 	}
 
