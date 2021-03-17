9e3ab5bf592ace3a0a2bb5605027a11e1eef5ac2
DenchikNO <44582128+DenchikNO@users.noreply.github.com>
2021-03-17 23:46:14 +0300

    Fix test version

--- src/main/java/com/touristBot/TouristBotApplication.java
+++ src/main/java/com/touristBot/TouristBotApplication.java
@@ -2,6 +2,7 @@ package com.touristBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class TouristBotApplication {
